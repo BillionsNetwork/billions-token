c4f19482f1220f91a2132ffb02b58d47d6c1ab9b
daveroga <daveroga@gmail.com>
2026-02-03 17:38:27 +0100

    fix package-lock.json

--- contracts/staking/interfaces/IStakingRewards.sol
+++ contracts/staking/interfaces/IStakingRewards.sol
@@ -60,7 +60,7 @@ interface IStakingRewards {
     /* ========== MUTATIVE FUNCTIONS ========== */
 
     function stake(uint256 amount) external;
-    
+
     function stakeOnBehalf(address account, uint256 amount) external;
 
     function withdraw(uint256 amount) external;
